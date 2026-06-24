f9ea1ff0c1d137f06cb80edfdbd56ff7e42e48e3
gil093 <gillanand.ag@gmail.com>
2026-06-24 16:28:27 -0700

    WIP: added support for multi child, additional refactoring

--- src/main/java/ca/vicilearning/dashboard/web/BrevoReviewTask.java
+++
@@ -1,9 +0,0 @@
-package ca.vicilearning.dashboard.web;
-
-public record BrevoReviewTask(
-    Long id,
-    String familyName,
-    String email,
-    String triggerReason,
-    Long templateId
-) {}
