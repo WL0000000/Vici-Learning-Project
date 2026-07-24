e979baff08e2f5d0f8ab08d432c4f5a1b9ac3709
AakiratAthwal <124299865+AakiratAthwal@users.noreply.github.com>
2026-07-23 17:17:26 -0700

    Initial controller setup, waiting to setup Clerk to complete this

--- src/main/java/ca/vicilearning/dashboard/web/UsersController.java
+++ src/main/java/ca/vicilearning/dashboard/web/UsersController.java
@@ -1,7 +1,19 @@
 // TODO: COMPLETE THIS
-
 package ca.vicilearning.dashboard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class UsersController {
     
+    @GetMapping("/admin/users")
+    public String userManagement(Model model) {
+
+        // TODO:
+        // model.addAttribute("users", userService.findAll());
+
+        return "dashboard-users";
+    }
+
 }
